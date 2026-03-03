2a60841ab0d112d6067dcecbb9bf675f74b1e10d
CleanBread <gleb4ist@gmail.com>
2026-03-03 13:32:51 +0100

    start

--- src/templates/trendle-trading/index.ts
+++ src/templates/trendle-trading/index.ts
@@ -5,20 +5,16 @@ import { type Template, getFile, getBase64Image, downloadImage } from '../../uti
 
 const cardTypes = {
   'profit': {
-    rightImg: 'images/profit.png',
     bgColor: '#72FF4B',
   },
   'loss': {
-    rightImg: 'images/loss.png',
     bgColor: '#FF604B',
   }
 }
 
-type Person = 'obama' | 'drake' | 'crash' | 'sydney' | 'tramp' | 'elon' | 'lewis' | 'mark' | 'powell' | 'macron' | 'arnold' | 'person-1' | 'tramp-2' | 'elon-2' | 'person-2' | 'taylor'
 export type Props = {
-  type: Person
   trend: {
-    image: string
+    image: string | undefined
     title: string
   }
   position: {
@@ -35,30 +31,25 @@ const template: Template<Props> = {
   height: 630,
   type: 'jpeg',
   html: async (props) => {
-    const { type, trend, pnl, position } = props
+    const { trend, pnl, position } = props
 
     const isProfit = pnl > 0
-    const { rightImg, bgColor } = isProfit ? cardTypes.profit : cardTypes.loss
+    const { bgColor } = isProfit ? cardTypes.profit : cardTypes.loss
     const { leverage, isLong, openLevel, exitLevel } = position
 
     const html = getFile(path.join(__dirname, 'index.html'))
 
     const logo = getBase64Image(path.resolve(__dirname, 'images/logo.png'))
-    const bgImage = getBase64Image(path.resolve(__dirname, 'images/bg.png'))
-    const rightImage = getBase64Image(path.resolve(__dirname, rightImg))
-    const personImage = getBase64Image(path.resolve(__dirname, `images/${type}.png`))
     const arrowImage = getBase64Image(path.resolve(__dirname, isLong ? 'images/arrow-up.png' : 'images/arrow-down.png'))
 
-    const trendImage = await downloadImage(trend.image)
+    const trendImage = trend.image ? await downloadImage(trend.image) : getBase64Image(path.resolve(__dirname, 'images/default.png'))
 
     const positionInfo = `${isLong ? ' Up' : ' Down'}, Boost x${leverage}`
 
     return html
       .replace('{logo}', logo)
       .replace('--card-bg-color', bgColor)
-      .replace('{bgImg}', bgImage)
-      .replace('{rightImg}', rightImage)
-      .replace('{personImg}', personImage)
+      .replace('{personImg}', trendImage)
       .replace('{trendImage}', trendImage)
       .replace('{trendTitle}', trend.title)
       .replace('{positionInfo}', positionInfo)
--- test/trendle-trading.ts
+++ test/trendle-trading.ts
@@ -2,7 +2,6 @@ import { Generator } from '../lib';
 import template from '../lib/templates/trendle-trading';
 
 const generator = new Generator()
-
 generator.run().then(() => {
   return Promise.all([
     generator.generate({
@@ -10,9 +9,8 @@ generator.run().then(() => {
       output: './test/images',
       filename: 'trendle-trading-loss-obama',
       props: {
-        type: 'obama',
         trend: {
-          image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
+          image: "https://content.trendle.fi/f86cfe79-e991-4046-bdfd-01eed76a22c2.png",
           title: 'Artificial intelligence',
         },
         position: {
@@ -22,293 +20,32 @@ generator.run().then(() => {
           exitLevel: 40.00,
         },
         pnl: -48.5,
+      },
+      options: {
+        waitUntil: 'networkidle0'
       }
     }),
     generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-loss-drake',
-    props: {
-      type: 'drake',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
+      template,
+      output: './test/images',
+      filename: 'trendle-trading-loss-obama-2',
+      props: {
+        trend: {
+          image: undefined,
+          title: 'Artificial intelligence',
+        },
+        position: {
+          leverage: 2,
+          isLong: true,
+          openLevel: 38.09,
+          exitLevel: 40.00,
+        },
+        pnl: -48.5,
       },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: -23.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-loss-crash',
-    props: {
-      type: 'crash',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: -23.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-loss-sydney',
-    props: {
-      type: 'sydney',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: -23.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-tramp',
-    props: {
-      type: 'tramp',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-elon',
-    props: {
-      type: 'elon',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-lewis',
-    props: {
-      type: 'lewis',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-mark',
-    props: {
-      type: 'mark',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-arnold',
-    props: {
-      type: 'arnold',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-elon-2',
-    props: {
-      type: 'elon-2',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-macron',
-    props: {
-      type: 'macron',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-person-1',
-    props: {
-      type: 'person-1',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-person-2',
-    props: {
-      type: 'person-2',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-powell',
-    props: {
-      type: 'powell',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-taylor',
-    props: {
-      type: 'taylor',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
-    generator.generate({
-    template,
-    output: './test/images',
-    filename: 'trendle-trading-profit-tramp-2',
-    props: {
-      type: 'tramp-2',
-      trend: {
-        image: "https://content.azuro.org/cc08578c-0334-4598-9d9d-15f8dc408e02.png",
-        title: 'Artificial intelligence',
-      },
-      position: {
-        leverage: 5,
-        isLong: false,
-        openLevel: 38.09,
-        exitLevel: undefined,
-      },
-      pnl: 13.1,
-    }
-  }),
+      options: {
+        waitUntil: 'networkidle0'
+      }
+    }),
   ]).finally(() => {
     generator.shutdown()
   })
